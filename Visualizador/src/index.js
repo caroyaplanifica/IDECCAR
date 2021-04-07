63d06dd841d3c135e31875eb51414722e1473774
jonhacastill <castillojonathane@gmail.com>
2021-04-07 13:38:08 -0300

    07-04-2021-13:35

--- Visualizador/src/index.js
+++ Visualizador/src/index.js
@@ -1,13 +1,24 @@
 const { Console } = require('console');
+
 const express = require('express');
+const engine = require('ejs-mate');
+const path = require('path');
+
+//Initialization
 const app = express();
 
-//middleware//
-app.get('/', (req, res) =>{
-    res.send('Hello World')
-})
+//Settings
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+//routes
+app.use(require('./routes/'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Starting the server
 app.listen(3000, () => {
-    console.log('Server on port 3000.')
-
-})
+    console.log('Server on port 3000.');
+});
---
+++ Visualizador/src/public/js/main.js
@@ -0,0 +1,6 @@
+const map = L.map('map-template').setView([-31.021380845522756, -64.06144005927875],13);
+        
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Castillo Jonathan</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+    }).addTo(map);
