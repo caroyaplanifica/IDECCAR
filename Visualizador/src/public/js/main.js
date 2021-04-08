3dc8de5291423c3f3309beeca8d944324d71f5bc
jonhacastill <castillojonathane@gmail.com>
2021-04-08 13:33:20 -0300

    08-04-2021-13:33

--- Visualizador/src/public/js/main.js
+++ Visualizador/src/public/js/main.js
@@ -1,4 +1,4 @@
 const map = L.map('map-template').setView([-31.0338,-64.1214],12);
         
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://tiles.wmflabs.org/osm-no-labels/${z}/${x}/${y}.png').addTo(map);
 
