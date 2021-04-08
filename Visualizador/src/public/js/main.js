c6dfa23bb54760870c2a355f0a480399ac7aa25b
jonhacastill <castillojonathane@gmail.com>
2021-04-08 12:46:18 -0300

    08-04-2021-12:46

--- Visualizador/src/public/js/main.js
+++ Visualizador/src/public/js/main.js
@@ -1,6 +1,4 @@
-var map = L.map('colcar').setView([-31.0338,-64.1214],12);
+const map = L.map('map-template').setView([-31.0338,-64.1214],12);
         
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
