f92e1092f36c7592a7d4a8a05246889758b7c08e
jonhacastill <castillojonathane@gmail.com>
2021-04-15 07:13:38 -0300

    15-04-2021-07:13

--- Visualizador/src/routes/index.js
+++ Visualizador/src/routes/index.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 
+//PostgreSQL and Postgis module and connection setup
+const {Client, Query} = require('pg');
+
+//Setup connection
+var username = "postgres"
+var password = "Postgis/ServidorIDE-Caroya"
+var host = "200.110.196.229:5433"
+var database = "db_sig"
+var conString = "postgres://"+username+":"+password+"@"+host+"/"+database;
+
 router.get('/', (req, res) =>{
     res.render('index');
 });
