cd80bb082eb21ad54d6a42108e1ebc780701abd5
jonhacastill <castillojonathane@gmail.com>
2021-04-16 10:26:43 -0300

    16-04-2021-10:26

--- Visualizador/src/public/js/main.js
+++ Visualizador/src/public/js/main.js
@@ -5,4 +5,7 @@ L.tileLayer('https://tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
-
+/*L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Castillo Jonathan</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);*/
