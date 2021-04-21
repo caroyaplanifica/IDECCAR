f2a9109b7a1f4ae24cda88d8b88ec7d1f14bf282
jonhacastill <castillojonathane@gmail.com>
2021-04-21 10:23:49 -0300

    21-04-2021-10:23

--- Visualizador/src/public/js/main.js
+++ Visualizador/src/public/js/main.js
@@ -9,9 +9,3 @@ L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Castillo Jonathan</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
-
-var ejido = L.ejido(mapDiv, mapOptions);
-
-var wmsLayer = L.tileLayer.wms('http://ows.mundialis.de/services/service?',{
-    layers: 'TOPO-OSM-WMS'
-}).addTo(ejido);
