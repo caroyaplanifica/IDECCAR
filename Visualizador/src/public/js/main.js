9909360861ab9a6bb9c65916547fadbd55571796
jonhacastill <castillojonathane@gmail.com>
2021-04-21 10:20:59 -0300

    21-04-2021-10:20

--- Visualizador/src/public/js/main.js
+++ Visualizador/src/public/js/main.js
@@ -1,25 +1,17 @@
 var map = L.map('map-template').setView([-31.055328, -64.060105], 12);
         
-L.tileLayer('https://tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Castillo Jonathan</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-}).addTo(map);
-
-/*L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+/*L.tileLayer('https://tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Castillo Jonathan</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);*/
 
-var popup = L.popup()
-    .setLatLng('-31.055328, -64.060105')
-    .setContent('<p>Municipio de <br />Colonia Caroya</p>')
-    .openOn(map);
-
-var ejido = L.tileLayer.wms('http://200.110.196.229:8080/geoserver/ideccar/wms',{
-    layers: 'ideccar:radio_municipal',
-    format: 'application/openlayers',
-    transparent: true,
-    version: '1.1.0',
+L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Castillo Jonathan</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
+
+var ejido = L.ejido(mapDiv, mapOptions);
+
+var wmsLayer = L.tileLayer.wms('http://ows.mundialis.de/services/service?',{
+    layers: 'TOPO-OSM-WMS'
+}).addTo(ejido);
