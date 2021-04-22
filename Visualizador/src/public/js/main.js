c29cb050807ee95da5f71477757d71db77727683
jonhacastill <castillojonathane@gmail.com>
2021-04-22 12:39:09 -0300

    22-04-2021-12:39

--- Visualizador/src/public/js/main.js
+++ Visualizador/src/public/js/main.js
@@ -9,3 +9,5 @@ L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">Castillo Jonathan</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
+
+var wms1 = L.tileLayer.wms()
